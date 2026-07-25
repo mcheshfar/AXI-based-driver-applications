f32302ab31fe7717f112809bbcace198e64a09aa
mcheshfar <mohammad.cheshfar@unica.it>
2026-07-25 15:51:12 +0200

    add tested vivado MDC-MLIR merged file code

---
+++ kv260/applications/edgedetection/MDC_MLIR_merged/vivado/acc_ip/drivers/src/s_accelerator.h
@@ -0,0 +1,27 @@
+/*****************************************************************************
+*  Filename:          s_accelerator.h
+*  Description:       Stream Accelerator Driver Header
+*  Date:              2026/07/25 15:38:30 (by Multi-Dataflow Composer - Platform Composer)
+*****************************************************************************/
+
+#ifndef S_ACCELERATOR_H
+#define S_ACCELERATOR_H
+
+/***************************** Include Files *******************************/		
+#include "xparameters.h"
+
+/************************** Constant Definitions ***************************/
+/************************* Functions Definitions ***************************/
+
+
+int s_accelerator_top1(
+	// port out0
+	int size_out0, int* data_out0,
+	// port in1
+	int size_in1, int* data_in1,
+	// port in0
+	int size_in0, int* data_in0
+);
+
+
+#endif /** MM_ACCELERATOR_H */
